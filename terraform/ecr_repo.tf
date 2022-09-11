a48e4ed032585098181be9b2260d5f41d7073ac4
fphilipp87 <federico.philipp@gmail.com>
2022-09-12 01:31:26 +0200

    terraform/plan/apply

--- terraform/ecr_repo.tf
+++ terraform/ecr_repo.tf
@@ -1,3 +1,3 @@
 resource "aws_ecr_repository" "springboot_repo" {
-  name = var.springboot_repo_name
+  name = dpvo-springboot
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,4 @@
-variable "springboot_repo_name" {
-  description = "Name of repository"
-  type        = string
-}
+
 
 
 
